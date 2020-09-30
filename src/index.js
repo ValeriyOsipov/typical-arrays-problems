11d24d7a33db10b49b6847af318d6ac22009cbdd
Valeriy Osipov <freebs@mail.ru>
2020-09-30 11:00:55 +0400

    init: start Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array === []) {
+    return 0;
+  }
+  return array.sort(function(a, b){return a - b})[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array === []) {
+    return 0;
+  }
+  return array.sort(function(a, b){return b - a})[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array === []) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum / array.length;
 }
